d2686817d71c73c9bb125a99fd7998f8fe7bf654
sreeraj-999 <sreerajep230@gmail.com>
2026-06-11 10:34:16 +0530

    Configure signed_cookies session engine for Vercel support

--- artisan_marketplace/artisan_marketplace/settings.py
+++ artisan_marketplace/artisan_marketplace/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Use cookie-based sessions to avoid writing session data to the read-only database
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
