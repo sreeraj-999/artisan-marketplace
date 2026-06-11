79ceb13ab743da69b5c20e6c0ad7695777682a69
sreeraj-999 <sreerajep230@gmail.com>
2026-06-11 10:18:59 +0530

    Update wsgi.py to expose app handler and add sys.path configuration

--- artisan_marketplace/artisan_marketplace/wsgi.py
+++ artisan_marketplace/artisan_marketplace/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for artisan_marketplace project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Add the Django project directory to Python path for serverless imports
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "artisan_marketplace.settings")
 
 application = get_wsgi_application()
+app = application
